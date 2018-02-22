db8ceac061c227677dd19b983542e4d551e2b8fb
Caleb Mech <cmech1@gmail.com>
2018-02-22 18:14:45 -0500

    Updated to use request-promise

--- server.js
+++ server.js
@@ -1,87 +1,114 @@
-var express = require('express');
-var bodyParser = require("body-parser");
-var request = require('request');
-var cheerio = require('cheerio');
-var app = express();
+const express = require('express');
+const bodyParser = require("body-parser");
+let rp = require('request-promise');
+const cheerio = require('cheerio');
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static('app'))
 
-var j = request.jar()
-request = request.defaults({jar: j})
+// Setup Cookie Jar
+let jar = rp.jar()
+rp = rp.defaults({ jar: jar })
 
-var loginPage = "https://mealacct.mcmaster.ca/OneWeb/Account/LogOn"
-var transactionPage = "https://mealacct.mcmaster.ca/OneWeb/Financial/TransactionsPass?dateFrom=2016%2F01%2F13+00%3A00%3A00&dateTo=2018%2F02%2F13+23%3A59%3A59&returnRows=1000&_=" + Date.now()
+// Setup Request Options
+const loginPage = "https://mealacct.mcmaster.ca/OneWeb/Account/LogOn"
+const transactionPage = "https://mealacct.mcmaster.ca/OneWeb/Financial/TransactionsPass?dateFrom=2016%2F01%2F13+00%3A00%3A00&dateTo=2018%2F02%2F13+23%3A59%3A59&returnRows=1000&_=" + Date.now()
 
+// Handle API Call
 app.post('/api', (req, res) => {
-    j = request.jar()
-    request = request.defaults({jar: j})
 
-    var token = 0;
-    var studentNumber = 0;
-    var accountPin = 0;
+	// Clear cookie jar on each request
+    jar = rp.jar()
 
-    var studentNumber = req.body.studentNumber;
-    var accountPin = req.body.accountPin;
+    let token = 0;
+    let studentNumber = req.body.studentNumber;
+	let accountPin = req.body.accountPin;
 
-    function sendResults(data) {
+	const getTokenOptions = {
+		uri: loginPage,
+		transform: (body) => {
+			return cheerio.load(body);
+		}
+	};
+
+	function sendResults(data) {
         res.send(data);
     }
+	
+	// Acquire Login Token
+	rp(getTokenOptions)
+	.then($ => {
+		token = $('[name=__RequestVerificationToken]').val();
+		params = { "__RequestVerificationToken": token, "Account": studentNumber, "Password": accountPin };
+		console.log("Request Token Acquired");
+		
+		const loginOptions = {
+			method: 'POST',
+			uri: loginPage,
+			form: params
+		}
+		
+		// Login to Payment System 
+		rp(loginOptions)
+		// Returns 200 if login is bad
+		.then(res => {
+			console.log("Incorrect student number / pin");
+		})
+		.catch((res, err) => {
+			if(res.statusCode == 302) {
+				console.log("Logged in");
 
-    request({url: loginPage}, (err, res, html) => {
-        
-        var $ = cheerio.load(html);
-        var token = $('[name=__RequestVerificationToken]').val();
-        var params = { "__RequestVerificationToken": token, "Account": studentNumber, "Password": accountPin };
-        console.log("Request Token Acquired")
-        
-        request.post({url: loginPage, form:params}, (err, res, html) => {
-            if(res.statusCode == 302) {
-                console.log("Logged in")
-                // console.log(html)
-                var html = null;
-                request({ url: transactionPage} , (err, res, html) => {
-                    console.log("Data downloaded")
-                    var trns = []
-                    var $ = cheerio.load(html);
-                    // console.log(token, studentNumber, accountPin, trns)
-    
-                    $('table.table > tbody > tr').each(function(i, elem) {
-    
-                        var dateTime = $(this).children('td[data-title="Date:"]').html()
-                        var date = dateTime.match(/\d{2}\/\d{2}\/\d{4}/g)[0];
-                        var time = dateTime.match(/\d{1,2}:\d{2}:\d{2} (?:PM|AM)/g)[0];
+				const scrapeOptions = {
+					uri: transactionPage,
+					transform: (body) => {
+						return cheerio.load(body);
+					}
+				}
+
+				// Scrape Transaction History
+				rp(scrapeOptions)
+				.then($ => {
+					let trns = [];
+
+					$('table.table > tbody > tr').each((i, elem) => {
+						
+						let dateTime = $(elem).children('td[data-title="Date:"]').html()
+                        let date = dateTime.match(/\d{2}\/\d{2}\/\d{4}/g)[0];
+                        let time = dateTime.match(/\d{1,2}:\d{2}:\d{2} (?:PM|AM)/g)[0];
     
                         trns[i] = { 
                             'date': date,
                             'time': time,
-                            'amount': $(this).children('td[data-title="Amount:"]').html().replace(',',''),
-                            'account': parseInt($(this).children('td[data-title="Balance:"]').html()),
-                            'desposit': parseInt($(this).children('td[data-title="Units:"]').html()),
-                            'trantype': $(this).children('td[data-title="Trantype:"]').html().trim(),
-                            'terminal': $(this).children('td[data-title="Terminal:"]').html().trim().slice(8), // Fix this hack with regex
+                            'amount': $(elem).children('td[data-title="Amount:"]').html().replace(',',''),
+                            'account': parseInt($(elem).children('td[data-title="Balance:"]').html()),
+                            'desposit': parseInt($(elem).children('td[data-title="Units:"]').html()),
+                            'trantype': $(elem).children('td[data-title="Trantype:"]').html().trim(),
+                            'terminal': $(elem).children('td[data-title="Terminal:"]').html().trim().slice(8), // Fix this hack with regex
                         }
                   });
     
-                  trns.reverse();
-                  var html = ""
-    
-                  console.log("Data processed")              
-                  sendResults(trns);
-    
-                });
-            } else {
-                console.error("Bad login")
-            }
-        });
-    });
+				  trns.reverse();
+				  
+				  console.log("Data processed");
+				  sendResults(trns);
+
+				})
+
+			} else {
+				console.error("Error logging in:\n", res.statusCode);
+			}
+
+		})
+	})
+	.catch(err => {
+		return console.error(err);
+	})
 
 });
 
-app.use(express.static('app'))
-
-var port = process.env.PORT || 3000
-
+const port = process.env.PORT || 3000
 app.listen(port)
+
 console.log('Magic happens on port', port);
-exports = module.exports = app;
